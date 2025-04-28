c0f679342800ee05fe50c8992e715918273e12e5
Himanshu4732 <himanshukumar9507638964@gmail.com>
2025-04-28 21:48:17 +0530

    fixed error

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
     await mongoose.connect(`${process.env.MONGODB_URI}/Mr.KCV`);
@@ -12,4 +12,4 @@ const connectDB = async () => {
     });
 };
 
-export default connectDB;
+module.exports = connectDB;
