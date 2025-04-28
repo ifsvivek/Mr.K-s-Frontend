993943c67beef854c06f75d68a5202ba26c409ec
Keerthan A <keerthann004@gmail.com>
2025-04-28 23:25:33 +0530

    fixed errors

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,15 +1,10 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    await mongoose.connect(`${process.env.MONGODB_URI}/Mr.KCV`);
+    mongoose.connection.on('connected', () => console.log("Database Connected"))
 
-    mongoose.connection.on('connected', () => {
-        console.log("Database Connected");
-    });
+    await mongoose.connect(`${process.env.DB_CONNECT}/MrKCV`)
 
-    mongoose.connection.on('error', (err) => {
-        console.error("Database connection error:", err);
-    });
 };
 
-export default connectDB;
+module.exports = connectDB;
